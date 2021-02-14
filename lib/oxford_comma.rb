be0e7b29ea4aa0521cbcca83daec77499e3ac9cb
Ashley Salazar <ashleysalazar0902@gmail.com>
2021-02-14 17:29:30 -0600

    commit tree fa5e2758640e1d0d5b16b670e1ca3ea83958f090

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
+    if (array.size <= 1)
+        return array.join("")
+    elsif (array.size <= 2)
+        return array.insert(-2, "and").join(" ")
+    else
+        string = array.insert(-2, "and,").join(", ")
+        string[',,'] = ''
 
+        return string
+    end
 end
